a41958cf0bf7efe599658751d9013f44374c1045
Nick Karadzha <nick.karadzha@jenza.com>
2025-06-21 10:55:22 +0100

    Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Menu, Search, User } from 'react-feather';
-
-import { QUERIES } from '../../constants';
-
-import MaxWidthWrapper from '../MaxWidthWrapper';
-import Logo from '../Logo';
-import Button from '../Button';
+import styled from "styled-components";
+import { Menu, Search, User } from "react-feather";
+import { QUERIES } from "../../constants";
+import MaxWidthWrapper from "../MaxWidthWrapper";
+import Logo from "../Logo";
+import Button from "../Button";
 
 const Header = () => {
   return (
@@ -29,7 +26,21 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+
         <Logo />
+
+        <SubscribeButtonWrapper>
+          <Button>Subscribe</Button>
+          <Link>Already a subscriber?</Link>
+        </SubscribeButtonWrapper>
       </MainHeader>
     </header>
   );
@@ -39,6 +50,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -46,9 +61,30 @@ const Row = styled(MaxWidthWrapper)`
   justify-content: space-between;
 `;
 
+const MainHeader = styled(MaxWidthWrapper)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 32px;
+  margin-bottom: 48px;
+
+  @media ${QUERIES.desktopAndUp} {
+    align-items: baseline;
+    justify-content: space-between;
+  }
+`;
+
 const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
+
+  ${MainHeader} & {
+    display: none;
+
+    @media ${QUERIES.desktopAndUp} {
+      display: flex;
+    }
+  }
 
   /*
     FIX: Remove the inline spacing that comes with
@@ -59,12 +95,21 @@ const ActionGroup = styled.div`
   }
 `;
 
-const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+const SubscribeButtonWrapper = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+const Link = styled.a`
+  font-size: ${14 / 16}rem;
+  text-decoration: underline;
+  font-style: italic;
 `;
 
 export default Header;
