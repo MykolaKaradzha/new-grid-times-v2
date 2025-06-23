9cf4937b7e939d56c4b339ea19d36bb0c659fddd
Nick Karadzha <nick.karadzha@jenza.com>
2025-06-23 19:40:44 +0100

    specialty story completed;

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { MARKET_DATA, SPORTS_STORIES } from '../../data';
+import { MARKET_DATA, SPORTS_STORIES } from "../../data";
 
-import MarketCard from '../MarketCard';
-import SectionTitle from '../SectionTitle';
-import MiniStory from '../MiniStory';
+import MarketCard from "../MarketCard";
+import SectionTitle from "../SectionTitle";
+import MiniStory from "../MiniStory";
+import { COLORS, QUERIES } from "../../constants";
 
 const SpecialtyStoryGrid = () => {
   return (
@@ -13,8 +14,8 @@ const SpecialtyStoryGrid = () => {
       <MarketsSection>
         <SectionTitle
           cornerLink={{
-            href: '/markets',
-            content: 'Visit Markets data »',
+            href: "/markets",
+            content: "Visit Markets data »",
           }}
         >
           Markets
@@ -25,18 +26,21 @@ const SpecialtyStoryGrid = () => {
           ))}
         </MarketCards>
       </MarketsSection>
+
       <SportsSection>
         <SectionTitle
           cornerLink={{
-            href: '/sports',
-            content: 'Visit Sports page »',
+            href: "/sports",
+            content: "Visit Sports page »",
           }}
         >
           Sports
         </SectionTitle>
         <SportsStories>
           {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
+            <SportStoryWrapper>
+              <MiniStory key={data.id} {...data} />
+            </SportStoryWrapper>
           ))}
         </SportsStories>
       </SportsSection>
@@ -47,14 +51,48 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+
+  @media ${QUERIES.tabletAndUp} {
+    gap: 64px;
+  }
+
+    @media ${QUERIES.laptopAndUp} {
+    gap: 0;
+    grid-template-columns: 1fr 1fr;
+  }
 `;
 
 const MarketsSection = styled.section``;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(min(165px, 100%), 1fr));
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+  border-left: 1px solid ${COLORS.gray[300]};
+  padding-left: 16px;
+  margin-left: 16px;
+  overflow: auto;
+`;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+
+  gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(min(165px, 100%), 1fr));
+
+  @media ${QUERIES.tabletAndUp} {
+    display: flex;
+    grid-template-columns: revert;
+  }
+`;
+
+const SportStoryWrapper = styled.div`
+  @media ${QUERIES.tabletAndUp} {
+    min-width: 220px;
+  }
+`;
 
 export default SpecialtyStoryGrid;
